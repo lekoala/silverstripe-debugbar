2720b49a3ad57c7fc69880378e095d828f98db75
Thomas <thomas@lekoala.be>
2016-09-17 11:42:37 +0200

    supercharge d() helper

--- _config.php
+++ _config.php
@@ -6,25 +6,78 @@ if (!defined('DEBUGBAR_DIR')) {
 // Add a simple utility that leverages Symfony VarDumper and cleans buffer to avoid debug messages
 if (!function_exists('d')) {
 
+    /**
+     * Helpful debugging helper. Pass as many arguments as you need.
+     * Keep the call on one line to be able to output arguments names
+     * 
+     * @return void
+     */
     function d()
     {
         // Clean buffer that may be in the way
         if (ob_get_contents()) ob_end_clean();
 
+        $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+
+        // Caller
+        $line = $bt[1]['line'];
+        $file = $bt[1]['file'];
+
+        // Probably best to avoid using this in live websites...
+        if (Director::isLive()) {
+            SS_Log::log("Please remove call to d() in $file:$line", SS_Log::WARN);
+            return;
+        }
+
+        // Arguments passed to the function are stored in matches
+        $src          = file($bt[0]["file"]);
+        $calling_line = $src[$bt[0]['line'] - 1];
+        preg_match("/d\((.+)\)/", $calling_line, $matches);
+
+        // Find all arguments, ignore variables within parenthesis
+        $arguments_name = [];
+        if (!empty($matches[1])) {
+            $arguments_name = array_map('trim',
+                preg_split("/(?![^(]*\)),/", $matches[1]));
+        }
+
+        $isAjax = Director::is_ajax();
+        $print  = function($v) use($isAjax) {
+            if (!$isAjax) {
+                $v = '<pre>'.$v.'</pre>';
+            }
+            if (is_string($v)) {
+                echo $v."\n";
+            } else {
+                echo print_r($v, true)."\n";
+            }
+        };
+
         // Display caller info
-        $caller = Debug::caller();
-        echo '<pre>'.$caller['file'].':'.$caller['line'].'</pre>'."\n";
+        $print("$file:$line\n");
 
         // Display data in a friendly manner
-        $args   = func_get_args();
-        $isAjax = Director::is_ajax();
+        $args = func_get_args();
+
+        $i = 0;
         foreach ($args as $arg) {
+            // Echo name of the variable
+            $len = 20;
+            if (isset($arguments_name[$i])) {
+                $print($arguments_name[$i]);
+                $len = strlen($arguments_name[$i]);
+            }
             // For ajax requests, a good old print_r is much better
             if ($isAjax || !function_exists('dump')) {
-                print_r($arg);
+                $print($arg);
+                // Make a nice line between variables for readability
+                if (count($args) > 1) {
+                    $print(str_repeat('-', $len));
+                }
             } else {
                 dump($arg);
             }
+            $i++;
         }
         exit();
     }
